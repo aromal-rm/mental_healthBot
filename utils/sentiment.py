d485e373f1012ad558c818ef36d28e6c4e2f619d
aromal-rm <aromalkalindhi@gmail.com>
2025-06-15 21:56:07 +0530

    NLTK fix

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,12 +1,14 @@
-# from textblob import TextBlob
 import nltk
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+
+required_nltk_packages = ['punkt', 'averaged_perceptron_tagger']
+
+for pkg in required_nltk_packages:
+    try:
+        nltk.data.find(f'tokenizers/{pkg}' if pkg == 'punkt' else f'taggers/{pkg}')
+    except LookupError:
+        nltk.download(pkg)
 
 from textblob import TextBlob
-
 
 def get_sentiment_score(text):
     blob = TextBlob(text)
